e9bbafb4dac2f0fcfa4a30a26ec4fd8e4673185f
YuYang <yy2012cn@gmail.com>
2014-09-06 22:04:06 +0800

    add makefile for computer at home

---
+++ home.mk
@@ -0,0 +1,48 @@
+CC = gcc
+AR = ar
+RANLIB = ranlib
+
+CFLAGS = -g -Wall -std=c99
+DEBUG = -DDEBUG
+
+OPENCL_INC_PATH = -I/usr/include
+OPENCL_LIB_PATH = -L/usr/lib/nvidia-current
+
+EXE_FILE = bsort
+SOURCES = bsort.c util.c clutil.c json.c log.c
+OBJS = bsort.o util.o clutil.o json.o log.o
+
+all: $(EXE_FILE)
+
+$(EXE_FILE): $(OBJS)
+	$(CC) $(OPENCL_LIB_PATH) -o $@ $^ -lOpenCL
+
+#自动处理头文件依赖
+#SOURCES为所有的源文件列表
+
+%.o:%.c
+	$(CC) -c $(CFLAGS) $(CPPFLAGS) $< -o $@ $(OPENCL_INC_PATH) $(DEBUG)
+
+# ignore the warn message "XXX.d: No such file or directory"
+-include $(SOURCES:.c=.d)
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) -MM $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+
+install: all
+	@if [ -d $(INSTDIR) ]; \
+	then \
+	cp myapp $(INSTDIR);\
+	chmod a+x $(INSTDIR)/$(EXE_FILE);\
+	chmod og-w $(INSTDIR)/$(EXE_FILE);\
+	echo “Installed in $(INSTDIR)“;\
+	else \
+	echo “Sorry, $(INSTDIR) does not exist”;\
+	fi
+
+.PHONY: clean
+clean:
+	-rm -f *.o $(EXE_FILE) *.d *.d.*
